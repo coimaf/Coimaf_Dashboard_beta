5661ea7b4025a67939adafb5f7f89de55f1f9258
Nicola Mazzaferro <nicola.mazzaferro@coimaf.lan>
2024-01-08 12:28:39 +0100

    test fix

--- resources/views/Errors/notFound.blade.php
+++ resources/views/Errors/notFound.blade.php
@@ -4,7 +4,7 @@
             <img width="150px" src="{{asset('assets/not_found.png')}}" alt="">
             <p class="fs-3"> <span class="text-warning">Opps!</span> Pagina non trovata.</p>
             <p class="lead">
-                La pagina che cercavi non esiste.
+                La pagina che cercavi non esiste
               </p>
             <a href="{{route('home')}}" class="btn btn-primary">Home</a>
         </div>
