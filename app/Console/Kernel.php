3d8e47bb0437d63ae03710888ecb21a6c148111b
Nicola Mazzaferro <nicolamazzaferro9@gmail.com>
2024-02-06 12:50:03 +0100

    Timer Notifiche

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:expired-deadline-notifications')->daily();
+        $schedule->command('send:expired-deadline-notifications')->everyMinute();
     }
     
 
