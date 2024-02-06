23dbebc0e94412abe426bf1fb6d931a8ac436907
Nicola Mazzaferro <nicolamazzaferro9@gmail.com>
2024-02-06 16:38:27 +0100

    Fix notification

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:expired-deadline-notifications')->dailyAt('16:30');
+        $schedule->command('send:expired-deadline-notifications')->dailyAt('17:00');
     }
     
 
