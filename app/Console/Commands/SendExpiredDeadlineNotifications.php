e7c577dcc8962c9bf9e81bf00206e52ccca5153a
Nicola Mazzaferro <nicolamazzaferro9@gmail.com>
2024-02-06 14:34:15 +0100

    fix notification

--- app/Console/Commands/SendExpiredDeadlineNotifications.php
+++ app/Console/Commands/SendExpiredDeadlineNotifications.php
@@ -13,29 +13,30 @@ class SendExpiredDeadlineNotifications extends Command
     protected $description = 'Send notifications for expired deadlines';
 
     public function handle()
-    {
-        $expiredDeadlines = Deadline::whereHas('documentDeadlines', function ($query) {
-            $query->where('expiry_date', '<', now());
-        })->get();
-    
-        foreach ($expiredDeadlines as $deadline) {
-            $cacheKey = 'scadenza_scaduta_' . $deadline->id;
-    
-            if (!Cache::has($cacheKey)) {
-                $recipients = collect([$deadline->user]);
-                if ($deadline->updated_by) {
-                    $recipients->push($deadline->updatedBy);
-                }
-    
-                foreach ($recipients as $recipient) {
-                    if ($recipient) {
-                        $recipient->notify(new \App\Notifications\ScadenzaScadutaNotification($deadline));
-                    }
-                }
-    
-                Cache::put($cacheKey, true, now()->endOfDay());
+{
+    $expiredDeadlines = Deadline::whereHas('documentDeadlines', function ($query) {
+        $query->where('expiry_date', '<', now());
+    })->get();
+
+    foreach ($expiredDeadlines as $deadline) {
+        $cacheKey = 'scadenza_scaduta_' . $deadline->id;
+
+        if (!Cache::has($cacheKey)) {
+            $recipients = collect([$deadline->user]);
+            if ($deadline->updated_by) {
+                $recipients->push($deadline->updatedBy);
             }
+
+            foreach ($recipients as $recipient) {
+                if ($recipient) {
+                    $expiryDate = $deadline->documentDeadlines->first()->expiry_date;
+                    $recipient->notify(new \App\Notifications\ScadenzaScadutaNotification($deadline, $expiryDate));
+                }
+            }
+
+            Cache::put($cacheKey, true, now()->endOfDay());
         }
     }
+}
     
 }
