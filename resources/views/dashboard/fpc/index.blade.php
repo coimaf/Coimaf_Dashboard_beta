e5d94b8e2b9603810baea46f315b1b55055cd9a0
NicolaMazzaferro <nicolamazzaferro9@gmail.com>
2024-02-19 18:29:40 +0100

    Change style index fpc

--- resources/views/dashboard/fpc/index.blade.php
+++ resources/views/dashboard/fpc/index.blade.php
@@ -1,20 +1,30 @@
 <x-Layouts.layoutDash>
-
+    
     <div class="row">
-        <div class="col-12 col-md-2 bg-white border border-3 text-center mx-auto d-flex justify-content-center align-items-center" style="height: 25vh;">
-            <h1>R1</h1>
+        <div class="col-12 col-md-2 bg-white border border-3 rounded-5 mt-5 text-center mx-auto d-flex justify-content-center align-items-center" style="height: 25vh;">
+            <a class="link-underline link-underline-opacity-0 link-dark" href="{{route('dashboard.fpc.r4.index')}}">
+                <i class="bi bi-journal-richtext"  style="font-size: 50px"></i><br/><br/>
+                <h1>R1</h1>
+            </a>
         </div>
-        <div class="col-12 col-md-2 bg-white border border-3 text-center mx-auto d-flex justify-content-center align-items-center" style="height: 25vh;">
-            <h1>R2</h1>
+        <div class="col-12 col-md-2 bg-white border border-3 rounded-5 mt-5 text-center mx-auto d-flex justify-content-center align-items-center" style="height: 25vh;">
+            <a class="link-underline link-underline-opacity-0 link-dark" href="{{route('dashboard.fpc.r4.index')}}">
+                <i class="bi bi-journal-richtext"  style="font-size: 50px"></i><br/><br/>
+                <h1>R2</h1>
+            </a>
         </div>
-        <div class="col-12 col-md-2 bg-white border border-3 text-center mx-auto d-flex justify-content-center align-items-center" style="height: 25vh;">
-            <h1>R3</h1>
+        <div class="col-12 col-md-2 bg-white border border-3 rounded-5 mt-5 text-center mx-auto d-flex justify-content-center align-items-center" style="height: 25vh;">
+            <a class="link-underline link-underline-opacity-0 link-dark" href="{{route('dashboard.fpc.r4.index')}}">
+                <i class="bi bi-journal-richtext"  style="font-size: 50px"></i><br/><br/>
+                <h1>R3</h1>
+            </a>
         </div>
-        <div class="col-12 col-md-2 bg-white border border-3 text-center mx-auto d-flex justify-content-center align-items-center" style="height: 25vh;">
-            <a href="{{route('dashboard.fpc.r4.index')}}">
+        <div class="col-12 col-md-2 bg-white border border-3 rounded-5 mt-5 text-center mx-auto d-flex justify-content-center align-items-center" style="height: 25vh;">
+            <a class="link-underline link-underline-opacity-0 link-dark" href="{{route('dashboard.fpc.r4.index')}}">
+                <i class="bi bi-journal-richtext"  style="font-size: 50px"></i><br/><br/>
                 <h1>R4</h1>
             </a>
         </div>
     </div>
-
+    
 </x-Layouts.layoutDash>
