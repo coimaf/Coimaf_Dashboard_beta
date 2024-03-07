a3262d791ac6ac16b9df3ee87af1f103fd12908d
Nicola Mazzaferro <nicolamazzaferro9@gmail.com>
2024-03-07 15:13:59 +0100

    fix 404NotFound

--- resources/views/Errors/notFound.blade.php
+++ resources/views/Errors/notFound.blade.php
@@ -2,11 +2,13 @@
     <div class="d-flex align-items-center justify-content-center vh-100">
         <div class="text-center">
             <img width="150px" src="{{asset('assets/not_found.png')}}" alt="">
-            <p class="fs-3"> <span class="text-warning">Opps!</span> Pagina non trovata.</p>
+            <p class="fs-3"> <span class="text-warning fw-bold fs-1">Opps!</span> Pagina non trovata.</p>
             <p class="lead">
                 La pagina che cercavi non esiste
-              </p>
-            <a href="{{route('dash')}}" class="btn btn-primary">Home</a>
+            </p>
+            <a href="{{route('dash')}}">
+                <button type="button" class="btn bg-primary-cust rounded rounded-3 text-white btn-cust fs-4">Home</button>
+            </a>
         </div>
     </div>
 </x-Layouts.layout>
